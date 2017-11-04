291ba6cfe794eaf72711f46f4f0ec8a49a5b501a
rizkyabdiperdana <rizkyabdiperdana10@gmail.com>
2017-11-04 23:49:42 +0700

    fungsi

--- main.py
+++ main.py
@@ -237,3 +237,67 @@ folium.Marker([-2.931131, 104.781986], popup= 'Dyskahijabpalembang').add_to(map_
 folium.Marker([-2.931238, 104.781820], popup= 'Martabak "Telor DOA IBU').add_to(map_1)
 
 map_1
+
+Fungsi Fungsi ;
+# Variabel global untuk menyimpan data Buku
+buku = []
+
+
+# fungsi untuk menampilkan semua data
+def show_data():
+    if len(buku) <= 0:
+        print "BELUM ADA DATA"
+    else:
+        for indeks in range(len(buku)):
+            print "[%d] %s" % (indeks, buku[indeks])
+
+
+# fungsi untuk menambah data
+def insert_data():
+    buku_baru = raw_input("Judul Buku: ")
+    buku.append(buku_baru)
+
+# fungsi untuk edit data
+def edit_data():
+	show_data()
+	indeks = input("Inputkan ID buku: ")
+	if(indeks > len(buku)):
+		print "ID salah"
+	else:
+		judul_baru = raw_input("Judul baru: ")
+		buku[indeks] = judul_baru
+
+# fungsi untuk menhapus data
+def delete_data():
+	show_data()
+	iindeksd = input("Inputkan ID buku: ")
+	if(indeks > len(buku)):
+		print "ID salah"
+	else:
+		buku.remove(buku[indeks])
+
+# fungsi untuk menampilkan menu
+def show_menu():
+	print "\n"
+	print "----------- MENU ----------"
+	print "[1] Show Data"
+	print "[2] Insert Data"
+	print "[3] Edit Data"
+	print "[4] Delete Data"
+	print "[5] Exit"
+	
+	menu = input("PILIH MENU> ")
+	print "\n"
+
+	if menu == 1:
+		show_data()
+	elif menu == 2:
+		insert_data()
+	elif menu == 3:
+		edit_data()
+	elif menu == 4:
+		delete_data()
+	elif menu == 5:
+		exit()
+	else:
+		print "Salah pilih!"
